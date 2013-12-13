4711848b38df1cbdb51bd56f5bbdda2e9e401cc4
Aqib Mushtaq <aqib@mushtaq.me>
2013-12-12 22:51:13 -0500

    Removed crap code

--- generator.js
+++ generator.js
@@ -1,7 +1,6 @@
 module.exports = function Generator () {
 
   var alphabet = "abcdefghijklmnopqrstuvwxyz";
-// var seed = "";
 
   function generateWord() {
     var word = "";
@@ -9,8 +8,6 @@ module.exports = function Generator () {
       word += alphabet.charAt(Math.floor(Math.random() * alphabet.length) + 1);
     return word;
   }
-  var word = generateWord();
-
 
   function getAdjacentNumbers (n, length) {
     var numbers = [];
@@ -18,18 +15,6 @@ module.exports = function Generator () {
       numbers.push({word: n+i});
     return numbers;
   };
-
-  function getHash (data) {
-    var hash = 0;
-    
-    if (data.length == 0) return hash;
-
-    for (i = 0; i < data.length; i++) {
-      c = data.charCodeAt(i);
-      hash = ((hash<<5)-hash)+c;
-      hash = hash & hash; // Convert to  bit integer
-    }
-  }
 
   this.getData = function (size) {
     return getAdjacentNumbers(generateWord(), size);
